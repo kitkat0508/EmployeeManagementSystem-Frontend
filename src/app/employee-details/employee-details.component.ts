31aa3ebcfcce76fd43b19ecfb8089d3448441e5a
EAS3COB <AnushKarthik.SenthilKumar@in.bosch.com>
2024-04-11 11:35:11 +0530

    Added Employee Details Component

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
+import { EmployeeService } from '../employee.service';
 import { Employee,employees } from '../model/Employee';
 
 @Component({
@@ -9,10 +11,33 @@ import { Employee,employees } from '../model/Employee';
 })
 export class EmployeeDetailsComponent implements OnInit {
   employee: Employee | undefined;
-  constructor() { }
+  employeeDetails: FormGroup;
+  employeeIdField: number | undefined;
+  constructor(private fb:FormBuilder,private employeeService:EmployeeService) { }
 
   ngOnInit() {
-    
+    this.employeeDetails = this.fb.group({
+      employeeId: ['', Validators.required],
+    });
+
+    this.employeeDetails.get('employeeId').valueChanges.pipe(
+      debounceTime(500)
+    ).subscribe((employeeId) => {
+      //console.log(employeeId);
+      this.employeeIdField=employeeId;
+      this.findEmployeeById(this.employeeIdField);
+    });
+
+  }
+
+  findEmployeeById(employeeId:number){
+      if(employeeId==null){
+        employeeId=0;
+      }
+      this.employeeService.getEmployeeById(employeeId).subscribe(
+        (employee)=>this.employee=employee,
+        (error) => console.error(error)
+      )
   }
 
 }
